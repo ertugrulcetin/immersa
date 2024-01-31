7a93a2b76585f18d3751184af495ed0c7ba5cb34
ertugrulcetin <ertu.ctn@gmail.com>
2024-01-31 22:24:39 +0300

    publish_editor.yml

--- sh/upload_editor_to_cf.sh
+++ sh/upload_editor_to_cf.sh
@@ -2,14 +2,19 @@
 
 rm -rf /Users/ertugrul/IdeaProjects/Immersa/resources/public/js/compiled
 
-npm run release
-
 current_timestamp=$(date +%s)000
 
 INDEX_PATH='/Users/ertugrul/IdeaProjects/Immersa/resources/public/index.html'
+ASSET_PATH='/Users/ertugrul/IdeaProjects/Immersa/resources/public'
 
+echo "Replacing __COMMIT_SHA__ with current timestamp"
 sed -i '' "s/__COMMIT_SHA__/${current_timestamp}/g" ${INDEX_PATH}
 
-npx wrangler pages deploy resources/public --project-name immersa-editor
+echo "Building editor"
+npm run release
 
+echo "Uploading editor to Cloudflare"
+npx wrangler pages deploy ${ASSET_PATH} --project-name immersa-editor
+
+echo "Reverting current timestamp to __COMMIT_SHA__"
 sed -i '' "s/${current_timestamp}/__COMMIT_SHA__/g" ${INDEX_PATH}
